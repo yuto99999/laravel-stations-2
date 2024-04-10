99fefcfb74e52b979024cb8b5aff2d9f27c71199
yuto99999 <myuto2003@gmail.com>
2024-04-10 17:47:48 +0900

    station1クリア

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,15 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('practice', function() {
+    return response('practice');
+});
+
+Route::get('practice2', function() {
+    return response('practice2');
+});
+
+Route::get('practice3', function () {
+    return response('test');
+});
