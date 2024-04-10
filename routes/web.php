62b99d3c0efab3fa4c94d42f0c04933ab0c68575
yuto99999 <myuto2003@gmail.com>
2024-04-10 18:28:47 +0900

    station3クリア

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,7 @@ use App\Http\Controllers\PracticeController;
 |
 */
 
+// Route::get('URL', [Controllerの名前::class, 'Controller内のfunction名']);
 Route::get('/practice', [PracticeController::class, 'sample']);
 Route::get('/practice2', [PracticeController::class, 'sample2']);
 Route::get('/practice3', [PracticeController::class, 'sample3']);
